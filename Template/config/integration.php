56b4a21862f9dbd72403ff3db8392111d2fb739d
ahcened <169063048+ahcened@users.noreply.github.com>
2024-06-11 14:24:27 +0200

    Update integration.php

--- Template/config/integration.php
+++ Template/config/integration.php
@@ -1,9 +1,7 @@
-<h3><img src="<?= $this->url->dir() ?>plugins/RocketChat/googlesheet-icon.png"/>&nbsp;Google Sheet</h3>
+<h3><img src="<?= $this->url->dir() ?>plugins/GoogleSheet/googlesheet-icon.png"/>&nbsp;Google Sheet</h3>
 <div class="panel">
-    <?= $this->form->label(t('Webhook URL profile configuration'), 'rocketchat_webhook_url') ?>
-    <?= $this->form->text('rocketchat_webhook_url', $values) ?>
-
-    <p class="form-help"><a href="https://github.com/kanboard/plugin-rocketchat#configuration" target="_blank"><?= t('Help on RocketChat integration') ?></a></p>
+    <?= $this->form->label(t('Webhook URL profile configuration'), 'googlesheet_webhook_url') ?>
+    <?= $this->form->text('googlesheet_webhook_url', $values) ?>
 
     <div class="form-actions">
         <input type="submit" value="<?= t('Save') ?>" class="btn btn-blue"/>
