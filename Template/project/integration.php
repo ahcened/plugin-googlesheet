0110bd1e0b09f5bb4ebd7bc6e885c07568bf98cc
ahcened <169063048+ahcened@users.noreply.github.com>
2024-06-11 14:25:56 +0200

    Update integration.php

--- Template/project/integration.php
+++ Template/project/integration.php
@@ -1,12 +1,10 @@
-<h3><img src="<?= $this->url->dir() ?>plugins/RocketChat/googlesheet-icon.png"/>&nbsp;Google Sheet</h3>
+<h3><img src="<?= $this->url->dir() ?>plugins/GoogleSheet/googlesheet-icon.png"/>&nbsp;Google Sheet</h3>
 <div class="panel">
-    <?= $this->form->label(t('Webhook URL for project'), 'rocketchat_webhook_url') ?>
-    <?= $this->form->text('rocketchat_webhook_url', $values) ?>
+    <?= $this->form->label(t('Webhook URL for project'), 'googlesheet_webhook_url') ?>
+    <?= $this->form->text('googlesheet_webhook_url', $values) ?>
 
-    <?= $this->form->label(t('Channel (Optional)'), 'rocketchat_webhook_channel') ?>
-    <?= $this->form->text('rocketchat_webhook_channel', $values, array(), array('placeholder="#channel"')) ?>
-    <p class="form-help"><a href="https://github.com/kanboard/plugin-rocketchat#configuration" target="_blank"><?= t('Help on RocketChat integration') ?></a></p>
-
+    <?= $this->form->label(t('Channel (Optional)'), 'googlesheet_webhook_channel') ?>
+    <?= $this->form->text('googlesheet_webhook_channel', $values, array(), array('placeholder="#channel"')) ?>
     <div class="form-actions">
         <input type="submit" value="<?= t('Save') ?>" class="btn btn-blue">
     </div>
