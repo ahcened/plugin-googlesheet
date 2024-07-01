c6737dfffcaad5e1cf75279e4912cc9a91f79d0e
ahcened <169063048+ahcened@users.noreply.github.com>
2024-07-01 16:34:29 +0200

    Update integration.php

--- Template/project/integration.php
+++ Template/project/integration.php
@@ -1,10 +1,8 @@
 <h3><img src="<?= $this->url->dir() ?>plugins/GoogleSheet/googlesheet-icon.png"/>&nbsp;Google Sheet</h3>
 <div class="panel">
-    <?= $this->form->label(t('Webhook URL for project'), 'googlesheet_webhook_url') ?>
+    <?= $this->form->label(t('Webhook URL'), 'googlesheet_webhook_url') ?>
     <?= $this->form->text('googlesheet_webhook_url', $values) ?>
 
-    <?= $this->form->label(t('Channel (Optional)'), 'googlesheet_webhook_channel') ?>
-    <?= $this->form->text('googlesheet_webhook_channel', $values, array(), array('placeholder="#channel"')) ?>
     <div class="form-actions">
         <input type="submit" value="<?= t('Save') ?>" class="btn btn-blue">
     </div>
