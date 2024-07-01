ba4963b137ce5943b7661329a7373f4d0c55dd9f
ahcened <169063048+ahcened@users.noreply.github.com>
2024-07-01 16:35:02 +0200

    Update integration.php

--- Template/user/integration.php
+++ Template/user/integration.php
@@ -3,9 +3,6 @@
     <?= $this->form->label(t('Webhook URL user'), 'googlesheet_webhook_url') ?>
     <?= $this->form->text('googlesheet_webhook_url', $values) ?>
 
-    <?= $this->form->label(t('User (Optional)'), 'googlesheet_webhook_channel') ?>
-    <?= $this->form->text('googlesheet_webhook_channel', $values, array(), array('placeholder="@username"')) ?>
-
     <div class="form-actions">
         <input type="submit" value="<?= t('Save') ?>" class="btn btn-blue">
     </div>
