f85da278aa8ca5f130aeb62ed2bd079018338cac
ahcened <169063048+ahcened@users.noreply.github.com>
2024-07-01 16:33:42 +0200

    Update integration.php

--- Template/config/integration.php
+++ Template/config/integration.php
@@ -1,6 +1,6 @@
 <h3><img src="<?= $this->url->dir() ?>plugins/GoogleSheet/googlesheet-icon.png"/>&nbsp;Google Sheet</h3>
 <div class="panel">
-    <?= $this->form->label(t('Webhook URL profile configuration'), 'googlesheet_webhook_url') ?>
+    <?= $this->form->label(t('Webhook URL'), 'googlesheet_webhook_url') ?>
     <?= $this->form->text('googlesheet_webhook_url', $values) ?>
 
     <div class="form-actions">
